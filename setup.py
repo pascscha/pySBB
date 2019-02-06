2c812502840292cc905f37cd21eee6bf7d4afdb5
Pascal Schärli <pascscha@student.ethz.ch>
2019-02-06 19:55:40 +0100

    Changed folder structure

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="sbpy",
+    name="pySBB",
     version="0.0.1",
     author="Pascal Schärli",
     author_email="pas.schaerli@sunrise.ch",
     description="An unofficial python wrapper for the SBB api.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pasch13/sbpy",
+    url="https://github.com/pasch13/pySBB",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
