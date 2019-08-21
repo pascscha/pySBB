79b456987f77233960d27bc30e4b941aeef4a9fe
Pascal Schärli <pas.schaerli@sunrise.ch>
2019-08-21 12:27:04 +0200

    Streamlined Parameter parsing

---
+++ pySBB/parameter_parser.py
@@ -0,0 +1,88 @@
+import datetime
+
+
+class Parameter:
+    def __init__(self, name, optional=True, default=None, valid_types=None, valid_values=None):
+        self.name = name
+        self.default = default
+        self.valid_types = valid_types
+
+    def parse(self, value):
+        if self.value is None:
+            if self.optional:
+                return None
+            else:
+                raise TypeError("{}: Non-Optional value cannot be None".format(self.name))
+
+        if self.valid_types is not None and type(value) not in self.valid_types:
+            expected_types = " or ".join([str(t) for t in self.valid_types])
+            raise TypeError("{}: Expected {} but got {} with type {}".format(self.name, expected_types, value, type(value)))
+        else:
+            processed_value = self.process(value)
+
+            if self.valid_values is not None and processed_value not in self.valid_values:
+                expected_values = " or ".join([str(v) for v in self.valid_values])
+                raise ValueError("{}: Expected {} but got {}".format(self.name, expected_values, processed_value))
+            else:
+                return processed_value
+
+    def process(self, value):
+        raise NotImplementedError("Please implement this method.")
+
+
+class ListParameter(Parameter):
+    def __init__(self, *args, element_parameter, min_length=None, max_length=None, **kwargs):
+        super().__init__(*args, valid_types=[list] + element_parameter.valid_types, **kwargs)
+        self.element_parameter = element_parameter
+        self.min_length = min_length
+        self.max_length = max_length
+
+    def process(self, value):
+        if type(value) is not list:
+            value = [value]
+        out = []
+        for elem in value:
+            out.append(self.element_parameter.parse(elem))
+
+        if self.min_length is not None and len(out) < self.min_length:
+            raise ValueError("{}: List length can't be smaller than {} but is {}".format(self.name, self.min_length, len(out)))
+        if self.max_length is not None and len(out) > self.max_length:
+            raise ValueError("{}: List length can't be bigger than {} but is {}".format(self.name, self.max_length, len(out)))
+
+        return out
+
+
+class BoolParameter(Parameter):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, valid_types=[bool], **kwargs)
+
+    def process(self, value):
+        if value:
+            return "1"
+        else:
+            return "0"
+
+
+class BoundedParameter(Parameter):
+    def __init__(self, *args, lower_bound=None, upper_bound=None, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.lower_bound = lower_bound
+        self.upper_bound = upper_bound
+
+    def process(self, value):
+        if self.lower_bound is not None and value < self.lower_bound:
+            raise ValueError("{}: Value can't be smaller than {} but is {}".format(self.name, self.lower_bound, value))
+        if self.upper_bound is not None and value < self.upper_bound:
+            raise ValueError("{}: Value can't be bigger than {} but is {}".format(self.name, self.upper_bound, value))
+        return str(value)
+
+
+class DateParameter(Parameter):
+    def __init__(self, *args, in_format="%Y-%m-%d", out_format="%Y-%m-%d", **kwargs):
+        super().__init__(*args, valid_types=[str], **kwargs)
+        self.in_format = in_format
+        self.out_format = out_format
+
+    def process(self, value):
+        datetime_object = datetime.strptime(value, self.in_format)
+        return datetime_object.strftime(self.out_format)
--- pySBB/query.py
+++ pySBB/query.py
@@ -6,81 +6,67 @@ import datetime
 
 from .utils import *
 from .classes import *
-
-url = "http://transport.opendata.ch/v1/"
-
-
-def get_connections(departure, arrival, via=None, date=None, time=None, isArrivalTime=False, transportations=None, limit=None, page=None, direct=None, sleeper=None, cauchette=None, bike=None, accessibility=None):
-
-    params = {"from": parse_str(departure),
-              "to": parse_str(arrival),
-              "via": parse_list(via),
-              "date": parse_date(date),
-              "time": parse_time(time),
-              "isArrivalTime": parse_bool(isArrivalTime),
-              "transportations": parse_restricted_list(transportations, ["train", "tram", "ship", "bus", "cableway"]),
-              "limit": parse_int(limit, lower_bound=1, upper_bound=16),
-              "page": parse_int(page, lower_bound=0, upper_bound=3),
-              "direct": parse_bool(direct),
-              "sleeper": parse_bool(sleeper),
-              "cauchette": parse_bool(cauchette),
-              "bike": parse_bool(bike),
-              "accessibility": parse_restricted_list(accessibility, ["independent_boarding", "assisted_boarding", "advanced_notice"])}
-
-    # add "[]" to the keys which have lists as values
-    array_entries = []
-    for key, value in params.items():
-        if type(value) == list and len(value) > 1:
-            array_entries.append((key, value))
-    for pair in array_entries:
-        key = pair[0]
-        value = pair[1]
-        params.pop(key)
-        params[key + "[]"] = value
-
-    response = requests.get(url + "connections", params=params)
-    json = response.json()
-    out = []
-    for conn in json["connections"]:
-        out.append(Connection(conn))
-    return out
+from .parameter_parser import *
 
 
-def get_locations(query=None, x=None, y=None, type="all"):
-    params = {"query": parse_str(query),
-              "x": parse_float(x),
-              "y": parse_float(y),
-              "type": parse_restricted(type, ["all", "station", "poi", "address"])
-              }
+class Query:
+    def __init__(self, url, return_class, *args, **kwargs):
+        self.url = url
+        self.args = args
+        self.kwargs = kwargs
 
-    response = requests.get(url + "locations", params=params)
-    json = response.json()
-    out = []
-    for loc in json["stations"]:
-        out.append(Location(loc))
-    return out
+    def __call__(self, *args, **kwargs):
+        params = {}
+        if len(args) != len(self.args):
+            raise ValueError("Expected {} arguments but got {}".format(len(self.args), len(args)))
+
+        for parser, value in zip(self.args, args):
+            if value is not None:
+                params[parser.name] = parser.parse(value)
+
+        for kw, value in kwargs.items():
+            if kw not in self.kwargs:
+                raise ValueError("Unexpected Keyword argument {}.".format(kw))
+            if value is not None:
+                parser = self.kwargs[kw]
+                params[parser.name] = parser.parse(value)
+
+        response = requests.get(self.url, params=params)
+
+        return return_class(response)
 
 
-def get_stationboard(station, id=None, limit=None, transportations=None, date=None, time=None, type="departure"):
-    if date is None:
-        date = datetime.datetime.now().strftime("%Y-%m-%d")    # 2012-03-25 17:30
-    else:
-        date = parse_date(date)
-    if time is None:
-        time = datetime.datetime.now().strftime("%H:%M")
-    else:
-        time = parse_time(time)
+base_url = "http://transport.opendata.ch/v1/"
 
-    params = {"station": parse_str(station),
-              "id": parse_int(id),
-              "limit": parse_int(limit),
-              "transportations": parse_restricted_list(transportations, ["train", "tram", "ship", "bus", "cableway"]),
-              "datetime": "{} {}".format(date, time),
-              "type": parse_restricted(type, ["departure", "arrival"])}
+get_connections = Query(base_url + "connections", Connections,
+                        Parameter("from", optional=False, valid_types=[str]),
+                        Parameter("to", optional=False, valid_types=[str]),
+                        via=ListParameter("via", Parameter("via", valid_types=[str])),
+                        date=DateParameter("date", in_format="%Y-%m-%d", out_format="%Y-%m-%d"),
+                        time=DateParameter("time", in_format="%H:%M", out_format="%H:%M"),
+                        isArrivalTime=BoolParameter("isArrivalTime"),
+                        transportations=ListParameter("transportations", Parameter("transportation", valid_values=["train", "tram", "ship", "bus", "cableway"])),
+                        limit=BoundedParameter("limit", lower_bound=1, upper_bound=15, valid_types=[int]),
+                        page=BoundedParameter("page", lower_bound=0, upper_bound=3, valid_types=[int]),
+                        direct=BoolParameter("direct"),
+                        sleeper=BoolParameter("sleeper"),
+                        cauchette=BoolParameter("cauchette"),
+                        bike=BoolParameter("bike"),
+                        accessibility=ListParameter("accessibility", Parameter("accessibility", valid_values=["independent_boarding", "assisted_boarding", "advanced_notice"]))
+                        )
 
-    response = requests.get(url + "stationboard", params=params)
-    json = response.json()
-    out = []
-    for stop in json["stationboard"]:
-        out.append(StationBoardEntry(stop))
-    return out
+get_locations = Query(base_url + "locations", Locations,
+                      query=Parameter("query", valid_types=[str]),
+                      x=Parameter("x", valid_types=[float]),
+                      y=Parameter("y", valid_types=[float]),
+                      type=Parameter("type", valid_values=["all", "station", "poi", "address"])
+                      )
+
+get_stationboard = Query(base_url + "stationboard", StationBoards,
+                         Parameter("station", valid_types=[str]),
+                         id=Parameter("id", valid_types=[int]),
+                         limit=BoundedParameter("limit", lower_bound=0),
+                         transportations=ListParameter("transportations", Parameter("transportation", valid_values=["train", "tram", "ship", "bus", "cableway"])),
+                         datetime=DateParameter("datetime", in_format="%Y-%m-%d %H:%M", out_format="%Y-%m-%d %H:%M"),
+                         type=Parameter("type", valid_values=["departure", "arrival"])
+                         )
